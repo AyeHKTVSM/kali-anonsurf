dcb4c44e7745edb9324c99bd23c82fc3e46c7f95
AyeHKTVSM <153189379+AyeHKTVSM@users.noreply.github.com>
2023-12-07 14:41:21 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,10 +10,10 @@ fi
 rm -f /etc/apt/sources.list.d/i2p.list
 
 # Compile the i2p ppa
-curl -fsSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x474bc46576fae76e97c1a1a1ab9660b9eb2cc88b' | sudo gpg --dearmor -o /etc/apt/keyrings/i2p.gpg # Import key 
-sudo chmod a+r /etc/apt/keyrings/i2p.gpg
-echo "deb [signed-by=/etc/apt/keyrings/i2p.gpg] https://ppa.launchpadcontent.net/i2p-maintainers/i2p/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/i2p.list
-echo "deb-src [signed-by=/etc/apt/keyrings/i2p.gpg] https://ppa.launchpadcontent.net/i2p-maintainers/i2p/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list.d/i2p.list
+ echo "deb [signed-by=/usr/share/keyrings/i2p-archive-keyring.gpg] https://deb.i2p2.de/ $(lsb_release -sc) main" \
+  | sudo tee /etc/apt/sources.list.d/i2p.list
+    curl -o i2p-archive-keyring.gpg https://geti2p.net/_static/i2p-archive-keyring.gpg
+      sudo cp i2p-archive-keyring.gpg /usr/share/keyrings
 apt-get update # Update repos
 
 if [[ -n $(cat /etc/os-release |grep kali) ]]
@@ -22,8 +22,9 @@ then
 	apt-get -f install # resolves anything else in a broken state
 fi
 
+apt-get install -y apt-transport-https lsb-release curl
 apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
-apt-get install -y secure-delete tor i2p # install dependencies, just in case
+apt-get install -y secure-delete tor i2p i2p-keyring # install dependencies, just in case
 
 # Configure and install the .deb
 dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
